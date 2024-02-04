21f21084dc006e8d0fb31028222746d7b150a669
Robelin09 <concepcionrobelin@gmail.com>
2024-02-03 23:23:45 -0400

    Creé la clase Estado

---
+++ PizzaPlace/Modelos/Estado.cs
@@ -0,0 +1,12 @@
+﻿
+namespace PizzaPlace.Modelos
+{
+    public class Estado
+    {
+        public Menu Menu { get; set; } = new Menu();
+        public CestadeCompra Cesta { get; } = new CestadeCompra();
+        public InterfazdeUsuario InterfazdeUsuario { get; set; } = new InterfazdeUsuario();
+
+        public decimal PrecioTotal => Cesta.Pedidos.Sum(id => Menu.ObtenerPizza(id)!.Precio);
+    }
+}
