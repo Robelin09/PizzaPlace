3f42f129a201ca9fc02435e848f4f5e83511628c
Robelin09 <concepcionrobelin@gmail.com>
2024-02-03 23:22:46 -0400

    Creé la clase Clientes

---
+++ PizzaPlace/Modelos/Clientes.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace PizzaPlace.Modelos
+{
+    public class Clientes
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Por favor, ingrese su nombre.")]
+        public string Nombre { get; set; } = default!;
+
+        [Required(ErrorMessage = "Proporcione una calle con el número de la casa.")]
+        public string Calle { get; set; } = default!;
+
+        [Required(ErrorMessage = "Proporcione su ciudad.")]
+        public string Ciudad { get; set; } = default!;
+    }
+}
