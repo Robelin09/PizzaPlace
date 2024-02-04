7e00f9545241a0b3965dc9e3813676c7000978cf
Robelin09 <concepcionrobelin@gmail.com>
2024-02-03 23:24:21 -0400

    Creé la clase Menu

---
+++ PizzaPlace/Modelos/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PizzaPlace.Modelos
+{
+    public class Menu
+    {
+        public List<Pizzas> Pizzas { get; set; }
+        = new List<Pizzas>();
+
+        public void Add(Pizzas pizzas) => Pizzas.Add(pizzas);
+
+        public Pizzas? ObtenerPizza(int id) => Pizzas.SingleOrDefault(pizzas => pizzas.Id == id);
+    }
+}
