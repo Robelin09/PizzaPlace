fa2bbaf33509f0aa2dd36301914f08c633851159
Robelin09 <concepcionrobelin@gmail.com>
2024-02-03 23:21:38 -0400

    Creé la clase Pizzas

---
+++ PizzaPlace/Modelos/Pizzas.cs
@@ -0,0 +1,17 @@
+﻿namespace PizzaPlace.Modelos
+{
+    public class Pizzas
+    {
+        public int Id { get;}
+        public string Nombre { get;}
+        public decimal Precio { get;}
+        public Picantes Picantes { get;}
+        public Pizzas(int id, string nombre, decimal precio, Picantes picantes)
+        {
+            this.Id = id;
+            this.Nombre = nombre;
+            this.Precio = precio;
+            this.Picantes = picantes;
+        }
+    }
+}
