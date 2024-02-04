ac5a79a7cc197ba4b0850ea1662f2ef6f0e52c63
Robelin09 <concepcionrobelin@gmail.com>
2024-02-03 23:22:36 -0400

    Creé la clase CestadeCompra

---
+++ PizzaPlace/Modelos/CestadeCompra.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace PizzaPlace.Modelos
+{
+    public class CestadeCompra
+    {
+        public Clientes Clientes { get; set; } = new Clientes();
+        public List<int> Pedidos { get; set; } = new List<int>();
+
+        public bool Pago { get; set; }
+
+        public void Add(int PizzaId) => Pedidos.Add(PizzaId);
+
+        public void EliminarEn(int pos) => Pedidos.RemoveAt(pos);
+    }
+}
